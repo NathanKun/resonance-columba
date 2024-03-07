68576143944801bcc4343ad8f7b1d326439de697
NathanKun <nathanhejunyang@gmail.com>
2024-03-07 09:02:12 +0000

    update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,13 +17,18 @@ export default function AboutPage() {
           项目目前还处于非常早期的阶段，还可能有大量的重构和改动，可能暂时不方便接受PR，但欢迎提交discussion和issue。
         </p>
         <p className="text-gray-600">项目目前仅实现了大部分共享Excel的功能，之后会逐步增加更多的功能。</p>
-        <p className="text-gray-600">感谢老黑的共享Excel https://www.kdocs.cn/l/cuPZppRuRJjO</p>
+        <p className="text-gray-600">
+          感谢
+          <Link href="https://space.bilibili.com/37824929">黑子不是黑Black</Link>
+          的共享Excel https://www.kdocs.cn/l/cuPZppRuRJjO
+        </p>
         <p className="text-gray-600">
           项目Github仓库:{" "}
           <Link href="https://github.com/nathankun/resonance-columba">
             https://github.com/nathankun/resonance-columba
           </Link>
         </p>
+        <p className="text-gray-600">雷索纳斯交流群 474679095</p>
       </div>
     </div>
   );
