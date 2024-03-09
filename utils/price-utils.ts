cd56fbe5cd58c0bc201f036205a0b681c8c82fc8
NathanKun <nathanhejunyang@gmail.com>
2024-03-09 21:28:58 +0000

    fix: remove old code which may make calculation inaccurate (pdt which have multiple buyable city)

--- utils/price-utils.ts
+++ utils/price-utils.ts
@@ -39,20 +39,15 @@ export const calculateProfit = (
 
   // for a buyable (non craftable) product
   if (!product.craft) {
-    // a product can be bought from multiple cities, so we need to find the city with the highest profit,
-    // but this will also make the calculate profit unclear which city it is from
-    for (const buyCity of Object.keys(product.buyPrices)) {
-      let productBuyPrice = product.buyPrices[buyCity] ?? 0;
-      const buyVariation = productPrices.buy?.[buyCity]?.variation ?? 0;
-      productBuyPrice = Math.round((productBuyPrice * buyVariation) / 100); // no bargin buy price
+    let productBuyPrice = product.buyPrices[sourceCity] ?? 0;
+    const buyVariation = productPrices.buy?.[sourceCity]?.variation ?? 0;
+    productBuyPrice = Math.round((productBuyPrice * buyVariation) / 100); // no bargin buy price
 
-      let productSellPrice = product.sellPrices[currentColumnCity] ?? 0;
-      const sellVariation = productPrices.sell?.[currentColumnCity]?.variation ?? 0;
-      productSellPrice = Math.round((productSellPrice * sellVariation) / 100); // no bargin sell price
+    let productSellPrice = product.sellPrices[currentColumnCity] ?? 0;
+    const sellVariation = productPrices.sell?.[currentColumnCity]?.variation ?? 0;
+    productSellPrice = Math.round((productSellPrice * sellVariation) / 100); // no bargin sell price
 
-      const cityProfit = Math.round(productSellPrice - productBuyPrice);
-      profit = Math.max(profit, cityProfit);
-    }
+    profit = Math.round(productSellPrice - productBuyPrice);
   }
   // a craftable product but with static price
   else if (product.craft.static) {
