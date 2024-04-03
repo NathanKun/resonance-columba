6fb9e68b547c30dff58c86393828d8e6e7ef863f
NathanKun <nathanhejunyang@gmail.com>
2024-04-03 18:32:58 +0200

    feat: add mui cssbaseline

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 // import { Analytics } from "@vercel/analytics/react";
 // import { SpeedInsights } from "@vercel/speed-insights/next";
 // import { GoogleAnalytics } from "@next/third-parties/google";
+import CssBaseline from "@mui/material/CssBaseline";
 import { StyledEngineProvider } from "@mui/material/styles";
 import { GoogleTagManager } from "@next/third-parties/google";
+import type { Metadata, Viewport } from "next";
 import { Noto_Sans_SC } from "next/font/google";
 import { env } from "process";
 import Header from "./components/header/header";
 import "./globals.css";
 import PriceProvider from "./price-provider";
-import type { Metadata, Viewport } from "next";
 
 export const metadata: Metadata = {
   title: "科伦巴商会",
@@ -40,6 +41,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" id="root">
       <StyledEngineProvider injectFirst>
+        <CssBaseline />
         <body className={`${inter.variable} min-h-screen m-0`}>
           <PriceProvider>
             <Header />
