0d26d874f39a1454499fb03f164cbff6ff9905f8
NathanKun <nathanhejunyang@gmail.com>
2024-04-06 12:16:32 +0200

    feat(data): remove low price product filter

--- data/Products.ts
+++ data/Products.ts
@@ -6,20 +6,5 @@ import {
 
 const pdts: Product[] = Object.values(pdtData) as Product[];
 
-export const PRODUCTS: Product[] = pdts.filter((product) => {
-  // exclude low buy price Normal products
-  if (product.type === "Normal") {
-    if (product.buyPrices) {
-      const buyPrices = Object.values(product.buyPrices).filter((price) => price !== null) as number[];
-      if (buyPrices.length === 0) {
-        return false;
-      }
-      if (Math.min(...buyPrices) < 300) {
-        return false;
-      }
-    }
-  }
-  return true;
-});
-
+export const PRODUCTS: Product[] = pdts;
 export const PRODUCT_UNLOCK_CONDITIONS: ProductUnlockConditions = goodsUnlockConditions;
