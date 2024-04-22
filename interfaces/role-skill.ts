94448d857fcf46b6636969f92303cd6ce605488d
NathanKun <nathanhejunyang@gmail.com>
2024-04-22 22:46:55 +0200

    feat: add resonance skills realated to bargain

--- interfaces/role-skill.ts
+++ interfaces/role-skill.ts
@@ -1,11 +1,22 @@
 export interface ResonanceSkill {
-  buyMore: {
+  buyMore?: {
     product?: {
       [pdtName: string]: number; // percentage, example: 10
     };
     city?: {
       [cityName: string]: number;
     };
+  };
+  bargain?: {
+    firstTrySuccessRate?: number; // + success rate for first bargain try
+    afterFailedSuccessRate?: number; // + success rate for after failed bargain try
+    bargainCount?: number; // + number of bargain tries
+    bargainSuccessRate?: number; // + success rate for bargain
+    bargainRate?: number; // + bargain rate
+    raiseCount?: number; // + number of raise tries
+    raiseSuccessRate?: number; // + success rate for raise
+    raiseRate?: number; // + raise rate
+    afterFailedLessFatigue?: number; // - fatigue after failed bargain
   };
 }
 
