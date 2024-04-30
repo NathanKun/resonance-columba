d1f2709241c11bd33a8071986cd8041da26d0030
神代綺凛 <i@loli.best>
2024-05-01 04:20:43 +0800

    feat: useLocalStorage

--- hooks/useSelectedCities.ts
+++ hooks/useSelectedCities.ts
@@ -1,54 +1,33 @@
 import { CITIES, CityName } from "@/data/Cities";
 import { SelectedCities } from "@/interfaces/prices-table";
 import { useCallback, useEffect, useState } from "react";
-
-const isServer = typeof window === "undefined";
+import { useLocalStorage } from "usehooks-ts";
 
 export default function useSelectedCities(props: { localStorageKey: string }) {
-  const { localStorageKey } = props;
   const initialSelectedCities = { sourceCities: [CITIES[0]], targetCities: [CITIES[1]] };
 
-  const [selectedCities, setSelectedCities] = useState<SelectedCities>(initialSelectedCities);
-
-  const initialize = () => {
-    if (isServer) {
-      return initialSelectedCities;
+  const [selectedCities, setSelectedCities] = useLocalStorage<SelectedCities>(
+    props.localStorageKey,
+    initialSelectedCities,
+    {
+      initializeWithValue: false,
     }
-    const selectedCitiesStr = localStorage.getItem(localStorageKey);
-    return selectedCitiesStr ? JSON.parse(selectedCitiesStr) : initialSelectedCities;
-  };
-
-  /* prevents hydration error so that state is only initialized after server is defined */
-  useEffect(() => {
-    if (!isServer) {
-      setSelectedCities(initialize());
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const updateSelectedCitiesLsAndState = useCallback(
-    (newSelectedCities: SelectedCities) => {
-      const newStr = JSON.stringify(newSelectedCities);
-      localStorage.setItem(localStorageKey, newStr);
-      setSelectedCities(newSelectedCities);
-    },
-    [localStorageKey]
   );
 
   const setSourceCities = useCallback(
     (selected: CityName[]) => {
       const newSelectedCities = { ...selectedCities, sourceCities: selected };
-      updateSelectedCitiesLsAndState(newSelectedCities);
+      setSelectedCities(newSelectedCities);
     },
-    [selectedCities, updateSelectedCitiesLsAndState]
+    [selectedCities, setSelectedCities]
   );
 
   const setTargetCities = useCallback(
     (selected: CityName[]) => {
       const newSelectedCities = { ...selectedCities, targetCities: selected };
-      updateSelectedCitiesLsAndState(newSelectedCities);
+      setSelectedCities(newSelectedCities);
     },
-    [selectedCities, updateSelectedCitiesLsAndState]
+    [selectedCities, setSelectedCities]
   );
 
   const switchSourceAndTargetCities = useCallback(() => {
@@ -56,8 +35,8 @@ export default function useSelectedCities(props: { localStorageKey: string }) {
       sourceCities: selectedCities.targetCities,
       targetCities: selectedCities.sourceCities,
     };
-    updateSelectedCitiesLsAndState(newSelectedCities);
-  }, [selectedCities, updateSelectedCitiesLsAndState]);
+    setSelectedCities(newSelectedCities);
+  }, [selectedCities, setSelectedCities]);
 
   return { selectedCities, setSourceCities, setTargetCities, switchSourceAndTargetCities };
 }
