a7822289a29d8399cc85927cee97c36989de613f
NathanKun <nathanhejunyang@gmail.com>
2024-08-11 11:37:05 +0200

    feat(event): active 红茶战争 event

--- data/Event.ts
+++ data/Event.ts
@@ -14,16 +14,16 @@ type GameEvent = ResonanceSkill & {
 export const EVENTS: GameEvent[] = [
   // 红茶战争 2024/03/21 - 2024/04/11
   // 红茶战争 2024/07/30 - 2024/08/20
-  // {
-  //   buyMore: {
-  //     product: {
-  //       红茶: 50,
-  //     },
-  //   },
-  //   taxVariation: {
-  //     product: {
-  //       红茶: -0.05,
-  //     },
-  //   },
-  // },
+  {
+    buyMore: {
+      product: {
+        红茶: 50,
+      },
+    },
+    taxVariation: {
+      product: {
+        红茶: -0.05,
+      },
+    },
+  },
 ];
